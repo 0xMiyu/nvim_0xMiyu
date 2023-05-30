b5d1b2ed2ed89cc619fce41a80a15247125515b2
0xMiyu <ong.martin24@gmail.com>
2023-05-31 00:22:32 +0800

    feat: set barbar keybinds

--- after/plugin/barbar.lua
+++ after/plugin/barbar.lua
@@ -56,3 +56,15 @@ require'barbar'.setup {
     Outline = {event = 'BufWinLeave', text = 'symbols-outline'},
   },
 }
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+map('n', '≤', '<Cmd>BufferPrevious<CR>', opts) --sending '<A-,>'
+map('n', '≥', '<Cmd>BufferNext<CR>', opts) --sending '<A-.>'
+-- Re-order to previous/next
+map('n', '…', '<Cmd>BufferMovePrevious<CR>', opts) --sending '<A-:>'
+map('n', 'æ', '<Cmd>BufferMoveNext<CR>', opts) --sending '<A-'>'
+
+
