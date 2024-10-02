2ede39c41f8eb0ef6038c8b90ad71ae1a4492938
0xMiyu <ong.martin24@gmail.com>
2024-10-02 16:22:43 +0800

    feat: shift barbar init options to lazy.lua and install neorg

--- lua/0xMiyu/lazy.lua
+++ lua/0xMiyu/lazy.lua
@@ -107,10 +107,62 @@ local plugins = {
          },
          init = function() vim.g.barbar_auto_setup = false end,
          opts = {
-           -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-           -- animation = true,
-           -- insert_at_start = true,
-           -- …etc.
+
+             animation = false,
+             clickable = true,
+             icons = {
+                 -- Configure the base icons on the bufferline.
+                 -- Valid options to display the buffer index and -number are `true`, 'superscript' and 'subscript'
+                 buffer_index = false,
+                 buffer_number = false,
+                 button = '',
+                 -- Enables / disables diagnostic symbols
+                 diagnostics = {
+                     [vim.diagnostic.severity.ERROR] = {enabled = true, icon = 'ﬀ'},
+                     [vim.diagnostic.severity.WARN] = {enabled = false},
+                     [vim.diagnostic.severity.INFO] = {enabled = false},
+                     [vim.diagnostic.severity.HINT] = {enabled = true},
+                 },
+                 gitsigns = {
+                     added = {enabled = true, icon = '+'},
+                     changed = {enabled = true, icon = '~'},
+                     deleted = {enabled = true, icon = '-'},
+                 },
+                 filetype = {
+                     -- Sets the icon's highlight group.
+                     -- If false, will use nvim-web-devicons colors
+                     custom_colors = true,
+
+                     -- Requires `nvim-web-devicons` if `true`
+                     enabled = true,
+                 },
+                 separator = {left = '▎', right = ''},
+
+                 -- Configure the icons on the bufferline when modified or pinned.
+                 -- Supports all the base icon options.
+                 modified = {button = '●'},
+                 pinned = {button = '', filename = true},
+
+                 -- Use a preconfigured buffer appearance— can be 'default', 'powerline', or 'slanted'
+                 preset = 'default',
+
+                 -- Configure the icons on the bufferline based on the visibility of a buffer.
+                 -- Supports all the base icon options, plus `modified` and `pinned`.
+                 alternate = {filetype = {enabled = false}},
+                 current = {buffer_index = true},
+                 inactive = {button = '×'},
+                 visible = {modified = {buffer_number = false}},
+             },
+             sidebar_filetypes = {
+                 -- Use the default values: {event = 'BufWinLeave', text = nil}
+                 NvimTree = true,
+                 -- Or, specify the text used for the offset:
+                 undotree = {text = 'undotree'},
+                 -- Or, specify the event which the sidebar executes when leaving:
+                 ['neo-tree'] = {event = 'BufWipeout'},
+                 -- Or, specify both
+                 Outline = {event = 'BufWinLeave', text = 'symbols-outline'},
+             },
          },
     },
 
@@ -124,6 +176,14 @@ local plugins = {
              'JoosepAlviste/nvim-ts-context-commentstring',
              'nvim-treesitter/nvim-treesitter'
          }
+    },
+
+    --neorg (orgmode for nvim)
+    {
+        "nvim-neorg/neorg",
+        lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+        version = "*", -- Pin Neorg to the latest stable release
+        config = true,
     },
    
 }
