149be7e532009f3ed5f250df9a1d4e1e00b5c089
0xMiyu <ong.martin24@gmail.com>
2024-10-02 16:39:25 +0800

    feat: uninstall neorg

--- lua/0xMiyu/lazy.lua
+++ lua/0xMiyu/lazy.lua
@@ -178,14 +178,6 @@ local plugins = {
          }
     },
 
-    --neorg (orgmode for nvim)
-    {
-        "nvim-neorg/neorg",
-        lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-        version = "*", -- Pin Neorg to the latest stable release
-        config = true,
-    },
-   
 }
 
 require("lazy").setup(plugins, {})
