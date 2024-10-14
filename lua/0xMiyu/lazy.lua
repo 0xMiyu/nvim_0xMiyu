57bb126d7e5cdec0843d918dfcbaa0223708259d
0xMiyu <ong.martin24@gmail.com>
2024-10-14 14:59:15 +0800

    feat: add mini surround

--- lua/0xMiyu/lazy.lua
+++ lua/0xMiyu/lazy.lua
@@ -210,7 +210,32 @@ local plugins = {
             vim.wo.conceallevel = 2
         end,
     },
+    
+    --surround tags
+   {
+       'echasnovski/mini.surround',
+       version = '*',
+       config = function()
+           require('mini.surround').setup {
+               -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+               highlight_duration = 500,
 
+               -- Module mappings. Use `''` (empty string) to disable one.
+               mappings = {
+                   add = 'ma', -- Add surrounding in Normal and Visual modes
+                   delete = 'md', -- Delete surrounding
+                   find = 'mf', -- Find surrounding (to the right)
+                   find_left = 'mF', -- Find surrounding (to the left)
+                   highlight = 'mh', -- Highlight surrounding
+                   replace = 'mr', -- Replace surrounding
+                   update_n_lines = 'mn', -- Update `n_lines`
+
+                   suffix_last = 'l', -- Suffix to search with "prev" method
+                   suffix_next = 'n', -- Suffix to search with "next" method
+               },
+           }
+       end,
+   }
 }
 
 require("lazy").setup(plugins, {})
