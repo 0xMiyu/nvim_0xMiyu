f32d5304420ab98f27edb9d8f96f920d911e0bf4
0xMiyu <ong.martin24@gmail.com>
2024-10-17 20:54:04 +0800

    feat: install markdown preview

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 My first attempt at customizing my own nvim setup, after multiple failed attempts at trying to switch to out-of-the-box solutions eg. Astro Nvim.
 
-Special thanks to the following 2 videos for being my main inspiration and guidance: 
+Special thanks to the following 2 videos for being my main inspiration and guidance:
 
 - [Devaslife's Neovim Setup](https://youtu.be/ajmK0ZNcM4Q)
 
--- lua/0xMiyu/plugins/lsp.lua
+++ lua/0xMiyu/plugins/lsp.lua
@@ -38,6 +38,26 @@ return {
 
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
+				settings = {
+					Lua = {
+						runtime = {
+							-- LuaJIT in the case of Neovim
+							version = "LuaJIT",
+							path = vim.split(package.path, ";"),
+						},
+						diagnostics = {
+							-- Get the language server to recognize the `vim` global
+							globals = { "vim" },
+						},
+						workspace = {
+							-- Make the server aware of Neovim runtime files
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
 			})
 
 			lspconfig.ts_ls.setup({
